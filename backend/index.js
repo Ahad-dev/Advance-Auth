68e256bfd5f6bb6f318fb2561295cb9851fc3f18
Muhammad Ahad <muhammadahad211126@gmail.com>
2024-08-26 09:24:07 +0500

    update

--- backend/index.js
+++ backend/index.js
@@ -9,18 +9,20 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT||5000;
 
-// Connect to the database
-connectDB();
 
 // Middleware
-// app.use(cors({
-//     origin: 'https://advance-auth-frontend.vercel.app'
-// }));
-app.options('*', cors({
-    origin: 'https://advance-auth-frontend.vercel.app'
+app.use(cors({
+    origin: 'https://advance-auth-frontend.vercel.app', // Only allow requests from this origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
 }));
+app.options('*', cors());
 app.use(express.json()); // for parsing application/json
 app.use(cookieParser()); // for parsing cookies
+
+
+// Connect to the database
+connectDB();
 
 // Routes
 
